da3877dfc12ee24159ad1eccde257198964c7cff
Maks Verver <maks@verver.ch>
2025-09-19 20:13:20 +0200

    Support randomizing initial pieces while keeping them on separate subsets of squares.
    This hides part of my strategy, though preliminary testing suggests this is
    worse than the static assignment I'd used previously. Good enough for early
    submissions.

--- player/src/player.cc
+++ player/src/player.cc
@@ -6,8 +6,10 @@
 #include <cassert>
 #include <chrono>
 #include <cstdlib>
+#include <iterator>
 #include <iostream>
 #include <optional>
+#include <ranges>
 #include <string>
 #include <string_view>
 #include <utility>
@@ -29,6 +31,11 @@ DECLARE_OPTION(std::string, arg_seed, "", "seed",
     "The chosen seed will be logged to stderr for reproducibility.");
 
 DECLARE_OPTION(int, arg_depth, 2, "depth", "Maximum search depth.");
+
+DECLARE_OPTION(int, arg_random_setup, 1, "random-setup",
+    "How to randomize starting pieces: 0=fixed layout "
+    "1=shuffle while avoiding pieces covering the same subset of squares "
+    "2=shuffle all");
 
 /*
 DECLARE_OPTION(int, arg_time_limit, LOCAL_BUILD ? 0 : 25, "time-limit",
@@ -181,15 +188,59 @@ std::pair<std::vector<Move>, int> FindBestMoves(State state, const std::vector<M
   return {best_moves, best_value};
 }
 
-SetupMove GenerateSetupMove(const State &state) {
-  // TODO: randomly distribute pieces while avoiding ALFIL/DABBABA/FERZ
-  // blocking each other.
+constexpr int SETUP_COUNT = 1 << 15;
+
+// Given an index between 0 and SETUP_COUNT (exclusive), generates the pieces
+// to set up, so that pieces of the same kind cover different subsets of the
+// squares on the board. See docs/setup.txt and tools/enum-setup.py for details.
+std::array<piece_t, 16> GenerateSetup(unsigned index) {
+  assert(index < SETUP_COUNT);
+  auto next_bit = [&index]() {
+    int res = index & 1;
+    index >>= 1;
+    return res;
+  };
+  std::array<piece_t, 16> pieces;
+  std::ranges::fill(pieces, WAZIR);
+  auto get_field = [&](const std::ranges::forward_range auto &opts) -> piece_t& {
+    auto values = opts | std::views::filter([&](int i) { return pieces[i] == WAZIR; });
+    assert(std::ranges::distance(values) == 2);
+    return pieces[*std::next(values.begin(), next_bit())];
+  };
+  get_field(std::array<int,  2>{  0,  4 }) = ALFIL;
+  get_field(std::array<int,  2>{  1,  5 }) = ALFIL;
+  get_field(std::array<int,  2>{  2,  6 }) = ALFIL;
+  get_field(std::array<int,  2>{  3,  7 }) = ALFIL;
+  get_field(std::array<int,  2>{  8, 12 }) = ALFIL;
+  get_field(std::array<int,  2>{  9, 13 }) = ALFIL;
+  get_field(std::array<int,  2>{ 10, 14 }) = ALFIL;
+  get_field(std::array<int,  2>{ 11, 15 }) = ALFIL;
+  get_field(std::array<int,  4>{  0,  2,  4,  6 }) = DABBABA;
+  get_field(std::array<int,  4>{  1,  3,  5,  7 }) = DABBABA;
+  get_field(std::array<int,  4>{  8, 10, 12, 14 }) = DABBABA;
+  get_field(std::array<int,  4>{  9, 11, 13, 15 }) = DABBABA;
+  get_field(std::array<int,  8>{  0,  2,  4,  6,  9, 11, 13, 15 }) = FERZ;
+  get_field(std::array<int,  8>{  1,  3,  5,  7,  8, 10, 12, 14 }) = FERZ;
+  get_field(std::array<int, 16>{  0,  1,  2,  3,  4,  5,  6,  7,
+                                  8,  9, 10, 11, 12, 13, 14, 15 }) = KNIGHT;
+  return pieces;
+}
+
+SetupMove GenerateSetupMove(const State &state, rng_t &rng) {
   std::array<piece_t, 16> pieces = {
       ALFIL,   ALFIL,   DABBABA, WAZIR,   KNIGHT,  DABBABA, ALFIL,   ALFIL,
       FERZ,    DABBABA, ALFIL,   ALFIL,   ALFIL,   ALFIL,   DABBABA, FERZ,
   };
-  if (state.NextPlayer() == BLUE) {
-    std::ranges::reverse(pieces);
+  if (arg_random_setup == 0) {
+    if (state.NextPlayer() == BLUE) {
+      std::ranges::reverse(pieces);
+    }
+  } else if (arg_random_setup == 1) {
+    std::ranges::shuffle(pieces, rng);
+  } else {
+    assert(arg_random_setup == 2);
+    std::uniform_int_distribution<unsigned> dist(0, SETUP_COUNT - 1);
+    pieces = GenerateSetup(dist(rng));
   }
   return SetupMove{.pieces = pieces};
 }
@@ -217,7 +268,7 @@ void PlayGame(rng_t &rng) {
       std::string output;
       if (state.turn < 2) {
         // Place initial pieces.
-        SetupMove setup_move = GenerateSetupMove(state);
+        SetupMove setup_move = GenerateSetupMove(state, rng);
         output = FormatSetupMove(state.NextPlayer(), setup_move);
         ExecuteSetupMove(state, setup_move);
       } else {
@@ -283,6 +334,14 @@ bool InitializeSeed(rng_seed_t &seed, std::string_view hex_string) {
 } // namespace
 
 int main(int argc, char *argv[]) {
+  // Debug-print semi-random setup moves.
+  if (false) {
+    for (int i = 0; i < SETUP_COUNT; ++i) {
+      std::cout << FormatSetupMove(RED, SetupMove{.pieces=GenerateSetup(i)}) << '\n';
+    }
+    return 0;
+  }
+
   LogId('R', player_name);
 
   if (!ParseOptions(argc, argv) || arg_help) {
