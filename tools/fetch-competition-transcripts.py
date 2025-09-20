19cd874a8f383af9f870462ff8dc712fda8e3740
Maks Verver <maks@verver.ch>
2025-09-20 16:27:04 +0200

    Add scripts to fetch competition game transcripts and my player logs.
    This is mostly copied from last year's code, with minor changes to match the
    expected data on the page.

---
+++ tools/fetch-competition-transcripts.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+import csv
+import json
+import re
+import os.path
+import sys
+import urllib.parse
+import urllib.request
+
+# GAME_RE is used to parse the game transcript from the HTML page.
+#
+# Example from https://www.codecup.nl/showgame.php?ga=313601
+#
+# <script type="application/javascript">let g = new ZeroPointOne(["Maks Verver","Nikita Kurgin"], ["Start"].concat(["AWADFAANDAFDAADA","adaadfadnaafdawa","a6c4","h1f2","b2d4","f2e4","b4b2","g3e5","a7c5","e5c3","d4f6","h8f6","c5e7","c3a5","b5d7","ac6","d7f5","h5f5","a4a6","c6a8","e7g5","h4g5","c4e6","g4e6","Dc7","ac4","b3c4","e6c4","Ae6","c4a2"]));</script><br>
+#
+# I want to extract the player names (to verify I have the right game), and the
+# list of moves played.
+GAME_RE = re.compile(r'<script type="application/javascript">let g = new ZeroPointOne\((\[.*?]), \["Start"].concat\((\[.*?])\)\);')
+SHOWGAME_BASE_URL = 'https://www.codecup.nl/showgame.php'
+PLAYER_NAMES = ['Red', 'Blue']
+
+class Game:
+  game_id: int
+  round: int
+  is_swiss: bool
+  user1: str|None
+  user2: str|None
+  score1: int|None
+  score2: int|None
+  status1: str|None
+  status2: str|None
+  moves: list[str]|None
+
+
+  def __init__(self, game_id, round, is_swiss):
+    self.game_id  = game_id
+    self.round    = round
+    self.is_swiss = is_swiss
+    self.user1    = None
+    self.score1   = None
+    self.status1  = None
+    self.user2    = None
+    self.score2   = None
+    self.status2  = None
+    self.moves    = None
+
+  def __str__(self):
+    return f'Game game_id={self.game_id} round={self.round} is_swiss={self.is_swiss} user1={self.user1} score1={self.score1} status1={self.status1} user2={self.user2} score2={self.score2} status2={self.status2}'
+
+
+def ReadCompetitionCsv(filename):
+  '''Returns a list of games in this competition.'''
+  games_by_id = {}
+  games = []
+  with open(filename, 'rt', newline='') as f:
+    for i, row in enumerate(csv.reader(f)):
+      if i == 0:
+        assert row == ['Game', 'Round', 'IsSwiss', 'Player', 'User', 'Score', 'Status']
+      else:
+        game_id, round, is_swiss, player, user, score, status = row
+        if game_id not in games_by_id:
+          game = Game(game_id, round, is_swiss)
+          games_by_id[game_id] = game
+          games.append(game)
+        else:
+          game = games_by_id[game_id]
+        if player == PLAYER_NAMES[0]:
+          assert game.user1 is None
+          game.user1 = user
+          game.score1 = int(score)
+          game.status1 = status
+        elif player == PLAYER_NAMES[1]:
+          assert game.user2 is None
+          game.user2 = user
+          game.score2 = int(score)
+          game.status2 = status
+        else:
+          print('Invalid player:', player)
+          assert False
+  return games
+
+
+def FetchGame(cachedir, game_id):
+  url = SHOWGAME_BASE_URL + "?ga=" + urllib.parse.quote(game_id)
+  cache_filename = os.path.join(cachedir, urllib.parse.quote_plus(url))
+  if os.path.exists(cache_filename):
+    # Read content from cache
+    with open(cache_filename, 'rt') as f:
+      text = f.read()
+  else:
+    # Fetch from internet
+    with urllib.request.urlopen(url) as f:
+      text = str(f.read(), 'utf-8')
+    # Write content to cache
+    with open(cache_filename, 'wt') as f:
+      f.write(text)
+
+  # Parse content (this should be in a separate function but whatever)
+  m = GAME_RE.search(text)
+  if m is None:
+    print('Skipping game', game_id, 'url:', url)
+    return None
+  users, moves = map(json.loads, m.groups())
+  assert len(users)
+  assert len(moves) > 0
+  assert all(isinstance(elem, str) for elem in users)
+  assert all(isinstance(elem, str) for elem in moves)
+  return (users, moves)
+
+
+def WriteAnnotatedCsv(filename, games):
+  with open(filename, 'wt', newline='') as f:
+    w = csv.writer(f, lineterminator='\n')
+    w.writerow(('Game', 'Round', 'IsSwiss', 'User1', 'Score1', 'Status1', 'User2', 'Score2', 'Status2', 'Moves'))
+    for game in games:
+      w.writerow((game.game_id, game.round, game.is_swiss, game.user1, game.score1, game.status1, game.user2, game.score2, game.status2, game.moves))
+
+
+if __name__ == '__main__':
+  if len(sys.argv) != 4:
+    print('Usage: %s <cachedir> <competition-csv> <games-csv>' % (sys.argv[0],))
+    sys.exit(1)
+  else:
+    _, cachedir, competition_csv, games_csv = sys.argv
+    assert os.path.isdir(cachedir)
+    games = ReadCompetitionCsv(competition_csv)
+    for i, game in enumerate(games):
+      print('%s (%d / %d)' % (game.game_id, i + 1, len(games)), file=sys.stderr)
+      assert game.moves is None
+      details = FetchGame(cachedir, game.game_id)
+      if details is not None:
+        users, moves = details
+        assert users == [game.user1, game.user2]
+        game.moves = ' '.join(moves)
+    WriteAnnotatedCsv(games_csv, games)
