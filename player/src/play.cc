e07f64f4d233249eab14a30ddf39ecb6c1bbb4e4
Maks Verver <maks@verver.ch>
2025-10-06 23:25:57 +0200

    Fix indentation.

--- player/src/play.cc
+++ player/src/play.cc
@@ -136,7 +136,7 @@ std::array<piece_t, 16> GenerateSetup(unsigned index) {
     get_field(std::array<int,  8>{  0,  2,  4,  6,  9, 11, 13, 15 }) = FERZ;
     get_field(std::array<int,  8>{  1,  3,  5,  7,  8, 10, 12, 14 }) = FERZ;
     get_field(std::array<int, 16>{  0,  1,  2,  3,  4,  5,  6,  7,
-                                                                    8,  9, 10, 11, 12, 13, 14, 15 }) = KNIGHT;
+                                    8,  9, 10, 11, 12, 13, 14, 15 }) = KNIGHT;
     return pieces;
 }
 
